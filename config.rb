3fda502ce9fcf3b620505b0ffee6e926dea438b6
mmwtsn <mwatson@dosomething.org>
2013-02-22 23:25:26 -0500

    Initial commit

---
+++ config.rb
@@ -0,0 +1,11 @@
+# compass configuration file
+
+http_path = "/"
+css_dir = "public/css"
+sass_dir = "public/sass"
+images_dir = "public/images"
+javascripts_dir = "public/scripts"
+
+output_style = :compressed
+
+preferred_syntax = :sass
