c1f0a6cc5f80ead1116002242f0abc51a192e57f
M. Maxwell Watson <hi@mmwtsn.com>
2013-08-13 18:12:38 -0400

    Tests, Bundler and some JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,41 @@
-// Fade site header + nav when reading
-$(window).scroll(function() {
+$(document).ready(function() {
 
-  $this = $(this);
-  $scrollTags = $('header, aside');
-  fadePoint = 80;
+  // Fade site header + nav when reading
+  $(window).scroll(function() {
 
-  if($this.scrollTop() > fadePoint) {
-    $scrollTags.fadeOut();
-  }
-  if($this.scrollTop() <= fadePoint) {
-    $scrollTags.fadeIn();
-  }
+    $this = $(this);
+    $scrollTags = $('header, aside');
+    fadePoint = 80;
+
+    if($this.scrollTop() > fadePoint) {
+      $scrollTags.fadeOut();
+    }
+    if($this.scrollTop() <= fadePoint) {
+      $scrollTags.fadeIn();
+    }
+  });
+
+  // Rotate symbol on hover
+  var $nav_button = $('#menu span');
+  var $nav = $('#nav');
+
+  $nav_button.hover(function() {
+    $nav_button.css({
+      'transform' : 'rotate(45deg)'
+    });
+    $nav.css({
+      'display' : 'block'
+    });
+  }, function() {
+    setTimeout(function(){
+      $nav_button.css({
+        'transform' : 'rotate(0deg)'
+      });
+      $nav.css({
+        'display' : 'none'
+      });
+    }, 500);
+  });
+
 });
 
