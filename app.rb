7a438753510fc530291e22a86ae16e47da580ff8
M. Maxwell Watson <hi@mmwtsn.com>
2013-09-01 14:14:12 -0400

    A mess of changes

--- app.rb
+++ app.rb
@@ -9,13 +9,30 @@ require './helpers'
 # Use default ERB layout wrapper for all markdown files
 set :markdown, :layout_engine => :erb, :layout => :layout
 
-# Index
-get "/" do
-  "Morning."
+# Static Routes
+get "/?" do
+  @class = 'home'
+  redirect '/musings'
+end
+
+get "/musings/?" do
+  @class = 'musings'
+  erb :musings
+end
+
+get "/about/?" do
+  @class = 'about'
+  erb :about
+end
+
+get "/work/?" do
+  @class = 'work'
+  erb :work
 end
 
 # Markdown routes
 get "/*/?" do
+  @class = "article"
   render_markdown
 end
 
