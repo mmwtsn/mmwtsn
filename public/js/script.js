97ba3413832da66fa3755e81908cdd96d7c25383
mmwtsn <mwatson@dosomething.org>
2013-09-02 14:10:17 -0400

    Some functionality

--- public/js/script.js
+++ public/js/script.js
@@ -3,17 +3,25 @@ $(document).ready(function() {
   // Fade site header + nav when reading
   //var chrome_fade = $(window).scroll(function() {
 
-    $this = $(this);
-    $scrollTags = $('header, aside');
-    fadePoint = 80;
+  $this = $(this);
+  $scrollTags = $('header, aside');
+  fadePoint = 80;
 
-    if($this.scrollTop() > fadePoint) {
-      $scrollTags.fadeOut();
-    }
-    if($this.scrollTop() <= fadePoint) {
-      $scrollTags.fadeIn();
-    }
+  if($this.scrollTop() > fadePoint) {
+    $scrollTags.fadeOut();
+  }
+  if($this.scrollTop() <= fadePoint) {
+    $scrollTags.fadeIn();
+  }
+
+  var border_width = function() {
+    page_width = $(window).width();
+    console.log(page_width);
+    $('#edge').css('border-left-width', page_width);
+  }
+  border_width();
+  $(window).resize(function() {
+    border_width();
   });
 
 });
-
