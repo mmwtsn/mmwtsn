2285a9081b0405fdd617c56c47accbebf46859f8
M. Maxwell Watson <hi@mmwtsn.com>
2013-09-29 00:31:25 -0400

    Functional animation

--- public/js/script.js
+++ public/js/script.js
@@ -25,30 +25,45 @@ $(document).ready(function() {
   });
 
   var toggleNav = function() {
-    $nav = $('nav');
-    $button = $('#toggle-nav');
+    var $nav = $('nav'); // This should be the entire sidebar, not just the nav
+    var $button = $('#toggle-nav');
+    var $article = $('.article');
 
     var start = {'left':'18.5rem'};
     var end   = {'left':'1rem'};
     var width = {'width':'toggle'};
+    var zero  = {'left':'0'};
+
+    // TODO - BUG
+    // If the "close" button is pressed immediately after
+    // the hover callback function is fired, the is(:hidden)
+    // calculation is done before the callback animation is
+    // complete. This causes a display error.
+
+    // TODO - IMPROVEMENT
+    // The "open" animation is not smooth. Each element
+    // slides in at a different rate.
 
     $button.click(function() {
       var $this = $(this);
       if($nav.is(':hidden')) {
         $button.animate(start, 250);
         $nav.animate(width, 250);
+        $article.animate(start, 250);
       }
       else {
         $button.animate(end, 250);
         $nav.animate(width, 250);
+        $article.animate(zero, 250);
       };
     });
 
     $nav.hover(function() {
       // On mouseEnter
     }, function() {
+      $(this).delay(500).animate(width, 250);
       $button.delay(500).animate(end, 350);
-      $(this).delay(500).animate(width, 350);
+      $article.delay(500).animate(zero, 350);
     });
   };
   toggleNav();
