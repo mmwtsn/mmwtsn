e6764c58299f7beee24a6a5cf72e74a5ec07a24b
M. Maxwell Watson <hi@mmwtsn.com>
2013-10-05 00:23:31 -0400

    Touches up JS

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,5 @@
 $(document).ready(function() {
 
-  // Fade site header + nav when reading
-  //var chrome_fade = $(window).scroll(function() {
-
-  $this = $(this);
-  $scrollTags = $('header, aside');
-  fadePoint = 80;
-
-  if($this.scrollTop() > fadePoint) {
-    $scrollTags.fadeOut();
-  }
-  if($this.scrollTop() <= fadePoint) {
-    $scrollTags.fadeIn();
-  }
-
-  var border_width = function() {
-    page_width = $(window).width();
-    $('#edge').css('border-left-width', page_width);
-  }
-  border_width();
-  $(window).resize(function() {
-    border_width();
-  });
-
   var toggleNav = function() {
     var $button  = $('#nav-toggle');
     var $aside   = $('aside');
@@ -39,34 +16,21 @@ $(document).ready(function() {
 
     var animating = false;
 
-    if(animating == false) {
-      $button.click(function() {
-        var asideClosed = $aside.hasClass('closed');
-        var $this = $(this);
+    $button.click(function() {
+      var asideClosed = $aside.hasClass('closed');
+      var $this = $(this);
 
-        if(asideClosed) {
-          $article.animate(article.open, 250);
-          $aside.animate(aside.open, 250);
-          $aside.toggleClass('closed');
-        }
-        else if(!asideClosed) {
-          $article.animate(article.close, 250);
-          $aside.animate(aside.close, 250);
-          $aside.toggleClass('closed');
-        }
-      });
-
-      //$nav.hover(function() {
-      //  // On mouseEnter
-      //  animating = true;
-      //}, function() {
-      //  $(this).delay(1000).animate(width, 250);
-      //  $article.delay(1000).animate(zero, 350);
-      //  $button.delay(1000).animate(end, 350, function() {
-      //    animating = false;
-      //  });
-      //});
-    }
+      if(asideClosed) {
+        $article.animate(article.open, 250);
+        $aside.animate(aside.open, 250);
+        $aside.toggleClass('closed');
+      }
+      else if(!asideClosed) {
+        $article.animate(article.close, 250);
+        $aside.animate(aside.close, 250);
+        $aside.toggleClass('closed');
+      }
+    });
   }
   toggleNav();
 
