7d6e914d351fba14252c2061692a18d34630c55a
M. Maxwell Watson <hi@mmwtsn.com>
2013-10-07 00:43:57 -0400

    It works!

--- public/js/script.js
+++ public/js/script.js
@@ -1,60 +1,26 @@
 $(document).ready(function() {
 
-  var toggleNav = function(sidebarClose, articleClose) {
+  var toggleSidebar = function() {
     var $button   = $('#nav-toggle');
     var $sidebar  = $('.sidebar');
     var $article  = $('.article');
-
-    var sidebar = {};
-    var article = {};
-
-    sidebar.open  = {'left' : '0px'};
-    sidebar.close = {'left' : '-250px'};
-
-    article.open  = {'left' : '264px'};
-    article.close = {'left' : '-16px'};
+    var $slideable = $('.sidebar, .article');
 
     $button.on('click touchstart', function() {
       var sidebarClosed = $sidebar.hasClass('closed');
-      var $this = $(this);
+
+      $slideable.toggleClass('closed');
 
       if(sidebarClosed) {
-        $article.animate(article.open, 250);
-        $sidebar.animate(sidebar.open, 250);
-        $sidebar.toggleClass('closed');
       }
-      else if(!sidebarClosed) {
-        if($(window).width() <= 600) {
-          $article.animate({'left':'11px'}, 250);
-          $sidebar.animate({'left':'-225px'}, 250);
-          $sidebar.toggleClass('closed');
-        }
-        if($(window).width() > 600) {
-          $article.animate(article.close, 250);
-          $sidebar.animate(sidebar.close, 250);
-          $sidebar.toggleClass('closed');
-        }
+      else if(!sidebarclosed) {
       }
     });
 
     if(window.location.hash === '#open') {
-      $article.css(article.open);
-      $sidebar.css(sidebar.open);
+      $slideable.toggleClass('closed');
     }
   }
-
-  var callToggleNav = function() {
-    var $window = $(window);
-    var breakpoint = 600;
-
-    if($window.width() > breakpoint) {
-      toggleNav('-250px', '-16px');
-    }
-
-    if($window.width() <= breakpoint) {
-      toggleNav('-225px', '11px');
-    }
-  }
-  callToggleNav();
+  toggleSidebar();
 
 });
