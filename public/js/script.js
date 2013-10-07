43fadaeff65ada0efada5703484fefaf42b2a98a
mmwtsn <mwatson@dosomething.org>
2013-10-07 00:45:45 -0400

    Tidies up JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -2,19 +2,10 @@ $(document).ready(function() {
 
   var toggleSidebar = function() {
     var $button   = $('#nav-toggle');
-    var $sidebar  = $('.sidebar');
-    var $article  = $('.article');
     var $slideable = $('.sidebar, .article');
 
     $button.on('click touchstart', function() {
-      var sidebarClosed = $sidebar.hasClass('closed');
-
       $slideable.toggleClass('closed');
-
-      if(sidebarClosed) {
-      }
-      else if(!sidebarclosed) {
-      }
     });
 
     if(window.location.hash === '#open') {
