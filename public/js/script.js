921143ed055f4d85987f7daad368d8ad2c86e5ac
mmwtsn <mwatson@dosomething.org>
2013-10-21 17:20:20 -0400

    Abstracts toggle from .article => .wrapper

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   var toggleSidebar = function() {
     var $button   = $('#nav-toggle');
-    var $slideable = $('.sidebar, .article');
+    var $slideable = $('.sidebar, .wrapper');
 
     $button.on('click touchstart', function() {
       $slideable.toggleClass('closed');
