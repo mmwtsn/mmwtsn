bdd8078fd7938744680e240f70808e5ea62f3b2e
mmwtsn <mwatson@dosomething.org>
2013-12-01 11:30:30 -0500

    Removes data passed to views and prints table of contents

--- app.rb
+++ app.rb
@@ -16,37 +16,22 @@ end
 
 # Words (blog), Work (portfolio) and About (personal infomration)
 get '/words/?' do
-  @title       = 'words'
-  @header      = 'Musings and Tutorials'
-  @sub_header  = 'This title isn\'t built yet.'
-
+  table_of_contents
   erb :words
 end
 
 get '/work/?' do
   @title      = 'work'
-  @header     = 'Recent Work'
-  @sub_header = 'Design and web development samples'
-
   erb :work
 end
 
 get '/about/?' do
-  @title       = 'about'
-  @class       = @title
-  @header      = 'About Maxwell'
-  @sub_header  = 'Full-stack web development and design'
-
   erb :about
 end
 
 # Markdown routes
 get '/*/?' do
   render_markdown
-
-  @title    = @header
-  @class    = 'musing'
-
   erb :musing
 end
 
