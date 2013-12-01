8a2aaa08069924319edafb2e027f6ae11275e259
mmwtsn <mwatson@dosomething.org>
2013-12-01 11:30:04 -0500

    First pass at method to build list of all posts

--- helpers.rb
+++ helpers.rb
@@ -19,12 +19,64 @@ helpers do
     @content = RDiscount.new(body).to_html
   end
 
+  # TODO def pin_open
+  # helper to allow users
+  # to keep sidebar toggled
+  # while viewing the site
+
   def header_image
     @header_image ? "style=\"background-image: #{ @header_image };\"" : '' 
   end
 
   def link
     @text ? "<a href=\"#{ @href }\">#{ @text }</a>" : ''
+  end
+
+  # TODO - "Post" is the conventional term for all blog writing;
+  # replace "musing" for "post" across the site to increase
+  # the code's readability
+
+  def parse_file (target, *args)
+    metadata = YAML.load(IO.read(target))
+
+    # Build the post URL from the filename
+    # by removing the filepath and file type
+    url = target + "/"
+    url.slice! "views"
+    url.slice! ".md"
+
+    # Create a new Date object to manipulate
+    d = Date.parse(metadata["date"])
+
+    # Format the datetime from the date
+    numerical_date = d.strftime("%D")
+    english_date   = d.strftime("%B %-d, %Y")
+    date_object    = d
+
+    # Ensure @musings is defined, otherwise create a new array
+    @musings = @musings || []
+
+    # Add current post to the unsorted array of posts as a hash
+    @musings.unshift({
+      :numerical_date => numerical_date,   # Used by datetime attribute of the HTML5 <time> element
+      :english_date   => english_date,     # Readable date printed on the page
+      :date_object    => date_object,      # Ruby Date object used for sorting the @musings array
+      :post_title     => metadata["title"],
+      :post_url       => url
+    })
+
+    # Sort each post by date to build a chronological list of posts
+    @musings.sort_by {|musing| musing[:date_object]}
+  end
+
+  def table_of_contents
+    musings = File.join("views/musings", "*.md")
+
+    all_musings = Dir.glob(musings)
+
+    all_musings.each do |musing|
+      parse_file musing, "title", "date"
+    end
   end
 
   def not_found
